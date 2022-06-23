eda5c47e8168307f8f3b19ccded1a06c81a6abd5
Marek Ruttner <marekruttner811@gmail.com>
2022-06-23 12:32:01 +0200

    utils.py

--- object_detection/raspberry_pi/utils.py
+++ object_detection/raspberry_pi/utils.py
@@ -24,21 +24,21 @@ def visualize(
     if detection not in _COLOR:
       _COLOR[detection] = list(np.random.randint(size=3) * 256)
 
-    for detection in detection_result[detection]:
-      # Draw bounding_box
-      bbox = detection.bounding_box
-      start_point = bbox.origin_x, bbox.origin_y
-      end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-      cv2.rectangle(image, start_point, end_point, _COLOR, 3)
+      for detection in detection_result[detection]:
+        # Draw bounding_box
+        bbox = detection.bounding_box
+        start_point = bbox.origin_x, bbox.origin_y
+        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+        cv2.rectangle(image, start_point, end_point, _COLOR, 3)
 
-      # Draw label and score
-      category = detection.classes[0]
-      class_name = category.class_name
-      probability = round(category.score, 2)
-      result_text = class_name + ' (' + str(probability) + ')'
-      text_location = (_MARGIN + bbox.origin_x,
-                      _MARGIN + _ROW_SIZE + bbox.origin_y)
-      cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                  _FONT_SIZE, _COLOR, _FONT_THICKNESS)
+        # Draw label and score
+        category = detection.classes[0]
+        class_name = category.class_name
+        probability = round(category.score, 2)
+        result_text = class_name + ' (' + str(probability) + ')'
+        text_location = (_MARGIN + bbox.origin_x,
+                        _MARGIN + _ROW_SIZE + bbox.origin_y)
+        cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                    _FONT_SIZE, _COLOR, _FONT_THICKNESS)
 
   return image
