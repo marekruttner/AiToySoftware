e594513ef206f7736d0bb3e9612e3b5684ebeffd
Marek Ruttner <45697300+marekruttner@users.noreply.github.com>
2022-06-23 12:55:28 +0200

    utils.py

--- object_detection/raspberry_pi/utils.py
+++ object_detection/raspberry_pi/utils.py
@@ -8,7 +8,7 @@ _MARGIN = 10  # pixels
 _ROW_SIZE = 10  # pixels
 _FONT_SIZE = 1
 _FONT_THICKNESS = 1
-_COLOR = {}
+_COLOR = list(np.random.random(size=3) * 255)
 """
 bar = ["0, 0, 255", "0, 255, 0", "255, 0, 0"]
 
@@ -21,24 +21,20 @@ def visualize(
 ) -> np.ndarray:
   
   for detection in detection_result.detections:
-    if detection not in _COLOR:
-      _COLOR[detection] = [randint(0, 255), randint(0, 255), randint(0, 255)]
+    # Draw bounding_box
+    bbox = detection.bounding_box
+    start_point = bbox.origin_x, bbox.origin_y
+    end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+    cv2.rectangle(image, start_point, end_point, _COLOR, 3)
 
-    for detection in detection_result[detection]:
-      # Draw bounding_box
-      bbox = detection.bounding_box
-      start_point = bbox.origin_x, bbox.origin_y
-      end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-      cv2.rectangle(image, start_point, end_point, _COLOR, 3)
-
-      # Draw label and score
-      category = detection.classes[0]
-      class_name = category.class_name
-      probability = round(category.score, 2)
-      result_text = class_name + ' (' + str(probability) + ')'
-      text_location = (_MARGIN + bbox.origin_x,
-                      _MARGIN + _ROW_SIZE + bbox.origin_y)
-      cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                  _FONT_SIZE, _COLOR, _FONT_THICKNESS)
+    # Draw label and score
+    category = detection.classes[0]
+    class_name = category.class_name
+    probability = round(category.score, 2)
+    result_text = class_name + ' (' + str(probability) + ')'
+    text_location = (_MARGIN + bbox.origin_x,
+                    _MARGIN + _ROW_SIZE + bbox.origin_y)
+    cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                _FONT_SIZE, _COLOR, _FONT_THICKNESS)
 
   return image
