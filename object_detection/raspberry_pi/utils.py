1db036dda455d4aa46bb4af2ab88ae479f6c95cb
Marek Ruttner <marekruttner811@gmail.com>
2022-07-08 21:28:09 +0200

    utils.py

--- object_detection/raspberry_pi/utils.py
+++ object_detection/raspberry_pi/utils.py
@@ -8,7 +8,7 @@ _MARGIN = 10  # pixels
 _ROW_SIZE = 10  # pixels
 _FONT_SIZE = 1
 _FONT_THICKNESS = 1
-_COLOR = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(class_name))]
+
 
 """list(np.random.random(size=3) * 255)""" """[classes[i]]""" 
 
@@ -32,6 +32,11 @@ def visualize(
 ) -> np.ndarray:
   
   for detection in detection_result.detections:
+
+    category = detection.classes[0]
+    class_name = category.class_name
+    _COLOR = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(class_name))]
+
     # Draw bounding_box
     bbox = detection.bounding_box
     start_point = bbox.origin_x, bbox.origin_y
@@ -39,8 +44,6 @@ def visualize(
     cv2.rectangle(image, start_point, end_point, _COLOR, 3)
 
     # Draw label and score
-    category = detection.classes[0]
-    class_name = category.class_name
     probability = round(category.score, 2)
     result_text = class_name + ' (' + str(probability) + ')'
     text_location = (_MARGIN + bbox.origin_x,
@@ -48,4 +51,4 @@ def visualize(
     cv2.putText(image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
                 _FONT_SIZE, _COLOR, _FONT_THICKNESS)
 
-  return image, class_name
+  return image
